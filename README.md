5235bee565b74c7c2d915a26e3f72ec719462f29
gregzanch <zanchelli.greg@gmail.com>
2024-11-19 10:51:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WaveTable
+
+WaveTable is a web-based synthesizer heavily inspired by [Serum](https://xferrecords.com/products/serum/).
---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import logo from "/logo.svg";
+  import Synth from "./lib/Synth.svelte";
+</script>
+
+<main>
+  <img src={logo} alt="" class="logo" />
+  <Synth />
+</main>
+
+<style>
+  .logo {
+    height: 8em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 1em rgba(0, 0, 0, 40%))
+  }
+</style>
