b90b0424f13ca36866065a30ea2a0b34fd2fe5b6
gregzanch <zanchelli.greg@gmail.com>
2024-12-02 22:57:27 -0500

    fixup user interface controls

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -7,6 +7,7 @@
   type ButtonProps = {
     bg?: string;
     fg?: string;
+    border?: string;
     onclick?: (e: MouseEvent) => void;
     children?: any;
     href?: string;
@@ -16,6 +17,7 @@
   let {
     bg = "var(--mono-darker)",
     fg = "var(--mono-lighter)",
+    border = "var(--mono-dark)",
     children = "button",
     onclick = () => {},
     href,
@@ -28,19 +30,20 @@
 
 {#if href}
   <a {href} target="_blank" rel="noopener noreferrer" aria-label={ariaLabel}>
-    <button class="btn" style:background-color={bg} style:color={fg} {onclick} style:cursor="pointer" {...props}>
+    <button class="btn" style:border-color={border} style:background-color={bg} style:color={fg} {onclick} style:cursor="pointer" {...props}>
       {@render children()}
     </button>
   </a>
 {:else}
-  <button class="btn" style:background-color={bg} style:color={fg} {onclick} {...props}>
+  <button class="btn" style:border-color={border} style:background-color={bg} style:color={fg} {onclick} {...props}>
     {@render children()}
   </button>
 {/if}
 
 <style>
   .btn {
-    border: none;
+    border-width: 1px;
+    border-style: solid;
     width: fit-content;
     padding: var(--spacing-200) var(--spacing-300);
     font-size: var(--font-size-default);
--- src/lib/DetuneControl.svelte
+++ src/lib/DetuneControl.svelte
@@ -20,16 +20,57 @@
 </script>
 
 
-<fieldset>
-  <legend>Detune</legend>
-  <label for="detune-down" hidden>Detune Down</label>
-  <Button id="detune-down" {onmousedown} {onmouseup}>{"<"}</Button>
-
+<fieldset aria-label="detune-label">
+  <div id="detune-label" class="detune-label">Detune</div>
+  <div class="left">
+    <label for="detune-down" hidden>Detune Down</label>
+    <Button id="detune-down" {onmousedown} {onmouseup} bg="var(--mono-lighter)" fg="var(--mono-darker)" border="var(--mono-light)">{"<"}</Button>
+  </div>
+<div class="right">
   <label for="detune-up" hidden>Detune Up</label>
-  <Button id="detune-up" {onmousedown} {onmouseup}>{">"}</Button>
+  <Button id="detune-up" {onmousedown} {onmouseup} bg="var(--mono-lighter)" fg="var(--mono-darker)" border="var(--mono-light)">{">"}</Button>
+</div>
 </fieldset>
 
 
 <style>
+  fieldset {
+    /* reset user agent styles */
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-block-start: 0em;
+    padding-inline-start: 0em;
+    padding-inline-end: 0em;
+    padding-block-end: 0em;
+    min-inline-size: min-content;
+    border-width: 0px;
+    border-style: none;
+    border-color: none;
+    border-image: initial;
+    /* implement our grid */
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr;
+    grid-template-rows: 1fr;
+    grid-column-gap: var(--spacing-100);
+    grid-row-gap: 0px;
+    grid-auto-flow: column;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+    justify-items: center;
+  }
+
+  div.detune-label {
+    color: var(--mono-darkest);
+    grid-area: 1 / 2 / 2 / 3;
+  }
+
+  div.left {
+    grid-area: 1 / 1 / 2 / 2;
+  }
+
+  div.right {
+    grid-area: 1 / 3 / 2 / 4;
+  }
 
 </style>
